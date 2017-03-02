0ebc35c614a66d1b8a88afbcb4bba22fabf61625
Dngn20 <dngn20@gmail.com>
2017-03-02 11:54:26 -0700

    added new GUI with new twitter stuff

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -15,7 +15,10 @@ public class ChatPanel extends JPanel
 	private JTextField chatField;
 	private SpringLayout baseLayout;
 	private JButton chatbotButton;
-	
+	private JButton SearchButton;
+	private JButton SentTweet;
+	private JButton SaveButton;
+	private JButton LoadButton;
 	
 	/**
 	 * Sets up the panel 
@@ -28,8 +31,12 @@ public class ChatPanel extends JPanel
 		baseLayout = new SpringLayout();
 		chatDisplay = new JTextArea(5, 25);
 		chatField = new JTextField(25);
-		chatButton = new JButton("Chat with the bot.");
-		chatbotButton = new JButton("This is Button");
+		chatButton = new JButton("Chat");
+		SentTweet = new JButton("Send a Tweet");
+		SaveButton = new JButton("Save");
+		LoadButton = new JButton("Load");
+		SearchButton = new JButton("Search Twitter");
+		
 		
 		setupPanel();
 		setupLayout();
@@ -48,6 +55,11 @@ public class ChatPanel extends JPanel
 		this.add(chatButton);
 		this.add(chatField);
 		this.add(chatbotButton);
+		this.add(SentTweet);
+		this.add(SaveButton);
+		this.add(LoadButton);
+		this.add(SearchButton);
+		
 	}
 	/**
 	 * Sets up the layout of the panel to make sure the objects are at the right place
@@ -56,12 +68,20 @@ public class ChatPanel extends JPanel
 	{
 		baseLayout.putConstraint(SpringLayout.SOUTH, chatDisplay, -163, SpringLayout.SOUTH, this);
 		baseLayout.putConstraint(SpringLayout.EAST, chatDisplay, -75, SpringLayout.EAST, this);
-		baseLayout.putConstraint(SpringLayout.NORTH, chatButton, 27, SpringLayout.SOUTH, chatField);
-		baseLayout.putConstraint(SpringLayout.EAST, chatButton, -138, SpringLayout.EAST, this);
 		baseLayout.putConstraint(SpringLayout.NORTH, chatField, 32, SpringLayout.SOUTH, chatDisplay);
 		baseLayout.putConstraint(SpringLayout.EAST, chatField, -65, SpringLayout.EAST, this);
 		baseLayout.putConstraint(SpringLayout.WEST, chatbotButton, 0, SpringLayout.WEST, chatButton);
 		baseLayout.putConstraint(SpringLayout.SOUTH, chatbotButton, -6, SpringLayout.NORTH, chatDisplay);
+		baseLayout.putConstraint(SpringLayout.NORTH, chatButton, 6, SpringLayout.SOUTH, chatField);
+		baseLayout.putConstraint(SpringLayout.WEST, chatButton, 43, SpringLayout.EAST, SentTweet);
+		baseLayout.putConstraint(SpringLayout.WEST, SentTweet, 10, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, SentTweet, 0, SpringLayout.NORTH, SaveButton);
+		baseLayout.putConstraint(SpringLayout.EAST, SaveButton, -36, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, LoadButton, 257, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, SaveButton, -6, SpringLayout.NORTH, LoadButton);
+		baseLayout.putConstraint(SpringLayout.WEST, LoadButton, 0, SpringLayout.WEST, SaveButton);
+		baseLayout.putConstraint(SpringLayout.NORTH, SearchButton, 6, SpringLayout.SOUTH, SentTweet);
+		baseLayout.putConstraint(SpringLayout.WEST, SearchButton, 0, SpringLayout.WEST, SentTweet);
 		
 	}
 	/**
